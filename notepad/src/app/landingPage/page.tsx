ee4b4da75e9c28eddce89068df04fe1255027071
Dawit Getachew <davewaanofii@gmail.com>
2025-03-18 11:20:33 +0300

    first commit

---
+++ notepad/src/app/landingPage/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <p>Get Start</p>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ notepad/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello world</p>
+      <Link href="/landingPage">Get Start</Link>
+    </div>
+  );
+}
