27af8bae3140b9df57df0caca324e64b2979b98f
Dawit Getachew <123627794+dave-get@users.noreply.github.com>
2025-04-02 09:43:53 +0300

    signup and signin page done (#6)

---
+++ notepad/src/app/api/auth/signup/page.tsx
@@ -0,0 +1,18 @@
+import Background from "@/components/background";
+import Signup from "@/components/form/authform/signup";
+import Headers from "@/components/header/headers";
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="flex flex-col mb-24 space-y-2">
+      <Headers />
+      <Background />
+      <div className="flex flex-col items-center">
+        <Signup />
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
--- notepad/src/components/background.tsx
+++ notepad/src/components/background.tsx
@@ -1,8 +1,26 @@
+"use client";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Background = () => {
+  const pathName = usePathname();
+  const isLogin = pathName === "/api/auth/login";
+  const isSignUp = pathName === "/api/auth/signup";
+  // const isContact = pathName === "/api/contact";
+  const isLandingPage = pathName === "/landingPage" || pathName === "/";
+  console.log(pathName);
   return (
-    <div className="absolute bg-gradient-to-b from-[#4D54FF] to-[#8460A8] blur-3xl bg-transparent shadow-2xl rounded-full h-[500px] w-[500px] flex justify-center items-center top-4/6 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-50"></div>
+    <div
+      className={`absolute bg-gradient-to-b from-[#4D54FF] to-[#8460A8] blur-3xl bg-transparent shadow-2xl rounded-full h-[500px] w-[500px] ${
+        isLandingPage
+          ? "top-4/6 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          : isSignUp
+          ? "-left-50 top-20"
+          : isLogin
+          ? "-right-50 top-20"
+          : ""
+      } -z-50`}
+    ></div>
   );
 };
 
