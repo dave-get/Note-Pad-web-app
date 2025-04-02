2ea71b68b7dd81680f50d1480ca1c86363c30ece
Dawit Getachew <davewaanofii@gmail.com>
2025-04-02 09:40:35 +0300

    signup and signin page done

---
+++ notepad/src/app/api/auth/login/page.tsx
@@ -0,0 +1,18 @@
+import Background from "@/components/background";
+import Login from "@/components/form/authform/login";
+import Headers from "@/components/header/headers";
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <div className="relative flex flex-col mb-24 space-y-2 overflow-x-clip">
+      <Headers />
+      <Background />
+      <div className="flex flex-col items-center">
+        <Login />
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
